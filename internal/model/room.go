65cfdcf0fcb6a2b2ff4c72f9c7b562fd555ef385
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-01-24 12:36:39 +0330

    feat: add room constructor

--- internal/model/room.go
+++ internal/model/room.go
@@ -1,10 +1,37 @@
 package model
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
+
+const idLength = 16
 
 type Room struct {
 	ID           string
 	HostID       string
 	LastActivity time.Time
 	CreatedAt    time.Time
+}
+
+func NewRoom(hostID string) *Room {
+	return &Room{
+		ID:           GenerateID(),
+		HostID:       hostID,
+		LastActivity: time.Now(),
+		CreatedAt:    time.Now(),
+	}
+}
+
+func GenerateID() string {
+	rand.Seed(time.Now().UnixNano())
+
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	id := make([]rune, idLength)
+
+	for i := range id {
+		id[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(id)
 }
