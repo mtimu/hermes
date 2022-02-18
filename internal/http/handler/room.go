23b915bc09535a54f0f37896e4a92011383b4083
Mohammadmehdi Teymorian <mehditeymorian322@gmail.com>
2022-02-18 12:30:08 +0330

    feat: add participant join handler

--- internal/http/handler/room.go
+++ internal/http/handler/room.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/mehditeymorian/hermes/internal/db/store"
@@ -20,6 +21,7 @@ type Room struct {
 func (r Room) Register(app *fiber.App) {
 	app.Get("/room/:id", r.get)
 	app.Post("/room", r.create)
+	app.Post("/room/:id", r.join)
 	app.Delete("/room/:id", r.del)
 }
 
@@ -80,6 +82,48 @@ func (r Room) create(ctx *fiber.Ctx) error {
 	r.Logger.Info("http.room.create", zap.String("status", "ok"))
 
 	return ctx.Status(http.StatusOK).JSON(room) //nolint:wrapcheck
+}
+
+// join a room
+func (r Room) join(ctx *fiber.Ctx) error {
+	roomID := ctx.Params("id")
+
+	r.Logger.Info("http.room.join", zap.String("room_id", roomID))
+
+	var req request.JoinRoom
+	if err := ctx.BodyParser(&req); err != nil {
+		r.Logger.Error("failed to parse body", zap.Error(err))
+
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{ //nolint:wrapcheck
+			"message": "failed to parse request body",
+			"error":   err.Error(),
+		})
+	}
+
+	room, err := r.Store.RoomCollection.Get(ctx.Context(), roomID)
+	if err != nil {
+		r.Logger.Error("failed to get the room", zap.Error(err), zap.String("room_id", roomID))
+
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{ //nolint:wrapcheck
+			"message": "failed to get the room",
+			"error":   err.Error(),
+		})
+	}
+
+	room.ParticipantsIDs = append(room.ParticipantsIDs, req.ParticipantID)
+	room.LastActivity = time.Now()
+
+	err = r.Store.RoomCollection.Update(ctx.Context(), *room)
+	if err != nil {
+		r.Logger.Error("failed to update room info", zap.Error(err))
+
+		return ctx.Status(http.StatusBadRequest).JSON(fiber.Map{ //nolint:wrapcheck
+			"message": "failed to add the participant",
+			"error":   err.Error(),
+		})
+	}
+
+	return ctx.SendStatus(http.StatusNoContent) //nolint:wrapcheck
 }
 
 // del deletes a room.
